7ac2350aee9040606c4138265d22e6e3f7084a7b
duyy35 <81275374+dh14072000@users.noreply.github.com>
2023-03-03 22:13:03 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'Cậu';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Chúc Mừng 8-3';
+const titleCard = 'Xù coder';
+const contentCard = 'Chúc BFF của tớ 8/3 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với cậu trong hôm nay và cả những ngày sau';
+
+const giftImage = 'cậu.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
