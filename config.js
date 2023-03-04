1c6a0ae6ecbddae8fc80d927688ac81a7d8752a7
duyy35 <81275374+dh14072000@users.noreply.github.com>
2023-03-04 16:50:21 +0700

    Mây

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
-const nameGirl = 'Cậu';
+const nameGirl = 'Mây';
 const giftUrl = 'http://nodemy.vn';
 const eventName = 'Chúc Mừng 8-3';
 const titleCard = 'Xù coder';
-const contentCard = 'Chúc BFF của tớ 8/3 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với cậu trong hôm nay và cả những ngày sau';
+const contentCard = 'Chúc Mây của tớ 8/3 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với cậu trong hôm nay và cả những ngày sau';
 
-const giftImage = 'cậu.png';
+const giftImage = 'Mây.png';
 const base64 = '';
 const giftImageBase64 = "data:image/png;base64, " + base64;
